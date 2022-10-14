52ad21600ee53eb0df4b6d3a6370beefd33745f2
Kaique <kaique.spagnol@gmail.com>
2022-10-14 12:12:24 -0300

    types, user validation, session next

--- backend/src/graphql/resolvers/user.ts
+++ backend/src/graphql/resolvers/user.ts
@@ -1,10 +1,53 @@
+import { CreateUsernameResponse, GraphQLContext } from "../../util/types";
 
 const resolvers = {
     Query: {
         searchUsers: () => {},
     },
     Mutation: {
-        createUsername: () => {},
+        createUsername: async (_: any, args: { username: string }, context: GraphQLContext): Promise<CreateUsernameResponse> => {
+            const { username } = args;
+            const { session, prisma } = context;
+            
+            if(!session?.user){
+                return {
+                    error: 'Acesso não autorizado'
+                }
+            }
+
+            const { id: userID } = session.user;
+            try {
+                
+                const existingUser = await prisma.user.findUnique({
+                    where: {
+                        username,
+                    },
+                });
+
+                if (existingUser){
+                    return{
+                        error: 'Username já está em uso, escolha outro',
+                    };
+                }
+
+                await prisma.user.update({
+                    where: {
+                        id: userID
+                    },
+                    data: {
+                        username
+                    }
+                });
+
+                return { seccess: true };
+
+            } catch (error: any) {
+                console.log("Erro ao criar o usuário: ", error);
+                return {
+                    error: error.message
+                }
+            }
+        },
     },
 };
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,18 +4,23 @@ import {
   ApolloServerPluginLandingPageLocalDefault,
 } from 'apollo-server-core';
 import { makeExecutableSchema } from '@graphql-tools/schema';
+import { getSession } from 'next-auth/react';
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 import https from 'https';
 import fs from 'fs';
 import typeDefs from './graphql/typeDefs';
 import resolvers from './graphql/resolvers';
+import * as dotenv from 'dotenv';
+import { GraphQLContext, Session } from './util/types';
 
 const httpsOptions = {
-  cert: fs.readFileSync('C:\\Users\\kaiqu\\Desktop\\comunicator\\backend\\ssl\\certs\\ca.crt'),
-  key: fs.readFileSync('C:\\Users\\kaiqu\\Desktop\\comunicator\\backend\\ssl\\certs\\ca.key')
+  cert: fs.readFileSync('C:\\Users\\kaiqu\\Desktop\\comunicator\\backend\\ssl\\certs\\blumchat.com.pem'),
+  key: fs.readFileSync('C:\\Users\\kaiqu\\Desktop\\comunicator\\backend\\ssl\\certs\\blumchat.com.key')
 }
 
 async function startApolloServer() {
+  dotenv.config();
   const app = express();
   const httpServer = https.createServer(httpsOptions, app);
 
@@ -24,17 +29,30 @@ async function startApolloServer() {
     resolvers,
   });
 
+  const corsOptions = {
+    origin: process.env.CLIENT_ORIGIN,
+    credentials: true,
+  }
+
+  const prisma = new PrismaClient();
+
+
   const server = new ApolloServer({
     schema,
     csrfPrevention: true,
     cache: 'bounded',
+    context: async ({ req, res }): Promise<GraphQLContext> => {
+      const session = await getSession({ req }) as Session;
+      return { session, prisma };
+    },
     plugins: [
       ApolloServerPluginDrainHttpServer({ httpServer }),
       ApolloServerPluginLandingPageLocalDefault({ embed: true }),
     ],
   });
+
   await server.start();
-  server.applyMiddleware({ app });
+  server.applyMiddleware({ app, cors: corsOptions });
   await new Promise<void>(resolve => httpServer.listen({ port: 4000 }, resolve));
   console.log(`🚀 Server ready at https://localhost:4000${server.graphqlPath}`);
 }
